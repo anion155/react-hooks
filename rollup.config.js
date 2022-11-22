cf8b46eb290b5bc77c4bb546b9aa74cdcf2d40f2
Mikhaylov Anton <anion155@gmail.com>
2022-11-22 10:19:23 +0300

    feat: initial commit

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import external from "rollup-plugin-peer-deps-external";
+
+import pkg from "./package.json";
+
+/** @type {import("rollup").Plugin[]} */
+const plugins = [
+  external({ includeDependencies: true }),
+  replace({
+    preventAssignment: true,
+    values: {
+      __DEV__: "(process.env.NODE_ENV !== 'production')",
+    },
+  }),
+  typescript(),
+  nodeResolve(),
+  commonjs(),
+];
+
+/** @type {import('rollup').MergedRollupOptions} */
+const config = {
+  input: pkg.exports["."].types,
+  output: {
+    file: pkg.exports["."].require,
+    format: "cjs",
+    sourcemap: true,
+  },
+  external: ["./internal"],
+  plugins,
+};
+
+export default config;
